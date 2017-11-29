cbee3b22042e0dea0a10c679de995e294bebd1a4
Rafi Barash <baras015@umn.edu>
2017-11-28 19:54:43 -0600

    Added StrategyOne

--- src/planetwars/publicapi/Driver.java
+++ src/planetwars/publicapi/Driver.java
@@ -1,8 +1,7 @@
 package planetwars.publicapi;
 
 import planetwars.core.GameWindow;
-import planetwars.strategies.NoOpStrategy;
-import planetwars.strategies.RandomMoveStrategy;
+import planetwars.strategies.*;
 
 import javax.swing.*;
 import java.io.FileNotFoundException;
@@ -12,8 +11,8 @@ public class Driver {
         try {
             // You can either instantiate the GameWindow by specifying two strategy classes directly, or by
             // using the default constructor and picking the strategies at run time
-            GameWindow window = new GameWindow();
-//            GameWindow window = new GameWindow(RandomMoveStrategy.class, NoOpStrategy.class);
+//            GameWindow window = new GameWindow();
+            GameWindow window = new GameWindow(RandomMoveStrategy.class, StrategyOne.class);
 
             window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             window.setVisible(true);
